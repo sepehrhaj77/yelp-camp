603cbd52e50d9053bfa584e8509026da3c9acf99
Sepehr Hajbarat <sepehrhaj77@gmail.com>
2021-09-23 12:28:13 -0400

    undoing last change

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,6 @@ module.exports.renderRegister = (req, res) => {
 module.exports.register = async (req, res, next) => {
     try {
         const { email, username, password } = req.body
-        username = username.toLowerCase()
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
         req.login(registeredUser, err => {
